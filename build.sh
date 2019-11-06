ecf4212ddd53b5622018df1337b3834354bf1f0b
SolarLune <solarlune@gmail.com>
2019-11-06 09:33:32 -0800

    Adding itch actions file for itch app. Pruning build script.

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
-go build -o ./bin/MasterPlan ./src/
+#!/bin/sh
 
-# Building on one's own system using the above command works fine, but cross-compilation doesn't work with vanilla Go
-# because I'm using raylib-go and CGO (C + Go) doesn't work with cross-compilation. So cross-compilation might be able to
-# work using techknowlogik's xgo fork.
-
-# sudo dockerd  # Run this in another terminal
-
-# gox -cgo -osarch="linux/amd64 windows/386" -output="bin/{{.OS}}_{{.Arch}}" ./src/
- 
-# sudo /home/solarlune/Documents/Projects/Go/bin/xgo -out MasterPlan --targets=linux/amd64 github.com/SolarLune/masterplan
-
-# CGO_ENABLED=1 CC_FOR_TARGET=gcc GOOS=windows GOARCH=amd64 go build -o ./bin/MasterPlan.exe ./src/
+go build -o ./bin/MasterPlan_linux.64 ./src/
+# env GOARCH=386 go build -o ./bin/MasterPlan.x64 ./src/
