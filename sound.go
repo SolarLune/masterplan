84d571cbaa92a2483e99fb287948b99708a66c4b
samaw <samaw@users.noreply.github.com>
2026-01-07 00:50:32 -0700

    Update sound.go

--- sound.go
+++ sound.go
@@ -190,7 +190,11 @@ var uiSounds = map[UISoundType][]string{}
 
 func init() {
 
-	filepath.Walk("assets/sounds/snddev_sine/", func(path string, info fs.FileInfo, err error) error {
+	filepath.Walk(LocalRelativePath("assets/sounds/snddev_sine/"), func(path string, info fs.FileInfo, err error) error {
+
+		if err != nil {
+			log.Fatalln("Error loading sound assets: ", err)
+		}
 
 		if info.IsDir() {
 			return nil
